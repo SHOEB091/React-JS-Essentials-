d6fa4634d24979558ca11263c84d1ba4a5b2087c
shoeb <wiqbal091@gmail.com>
2024-11-12 16:44:36 +0530

    currency Converter Done

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'preact/hooks'
+import './app.css'
 import Card from './components/Card'
-
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
-  let myObj = {
-    username: "hitesh",
-    age: 21
-  }
-  let newArr = [1, 2, 3]
 
-  return (
-    <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username="chaiaurcode" btnText="click me" />
-      <Card username="hitesh" />
+  let myObj={
+    username:"Shoeb",
+    age:21
+  }
+  let newArr = [1,2,3,4,5]
+
+
+  return ( //fragments are used to return multiple elements
+    <>  
+    <h1 className="bg-green-600 text-black p-4 rounded-xl mb-4">Tailwind test </h1>
+  
+   {/* suppose we want to use the card with dirrerent usernames and button text wo here we use props to pass the values */}
+    <div className='mb-5'>
+    <Card username='shoeb iqbal ' btnText='Join Us!'/>
+    </div>
+    <Card username='JLG shoeb' btnText="Follow me "/>
+
     </>
   )
 }
-
-export default App
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-function Card({username, btnText="visit me"}) {
-    console.log(username);
+
+//To avoid props.username is undefined, we can set a default value for the props  or we Destructuring the props in the function parameter
+function Card({ username = "Anonymous", btnText = "Visit Me" }) {
+  console.log(username); //visit Me is the default value 
+  
   return (
-    <div className="relative h-[400px] w-[300px] rounded-md ">
-  <img
-    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-    alt="AirMax Pro"
-    className="z-0 h-full w-full rounded-md object-cover"
-  />
-  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-  <div className="absolute bottom-4 left-4 text-left">
-    <h1 className="text-lg font-semibold text-white">{username}</h1>
-    <p className="mt-2 text-sm text-gray-300">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
-    </p>
-    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      {btnText } →
-    </button>
-  </div>
-</div>
-  )
+    <div className="relative h-[400px] w-[300px] rounded-md shadow-lg">
+      <img
+        src="https://images.unsplash.com/photo-1729582017869-a0c84b5c91b5?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="User"
+        className="z-0 h-full w-full rounded-md object-cover"
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div className="absolute bottom-4 left-4 text-left">
+        <h1 className="text-lg font-semibold text-white">{username}</h1>
+        <p className="mt-2 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+          debitis?
+        </p>
+        <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+          {btnText} →
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
